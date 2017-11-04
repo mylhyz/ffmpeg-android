480b3055e3deda93b3f9474ea981c0d1e2f2b280
mylhyz <mylhyz@gmail.com>
2017-11-04 22:05:14 +0800

    init the project with android/ffmpeg/build

---
+++ README.md
@@ -0,0 +1,3 @@
+# ffmpeg for android
+
+编译最新稳定版本的 ffmpeg ， 作为 android 开发使用
---
+++ build/build_ffmpeg_android.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+#
+# build ffmpeg for android only for arm64(armeabi-v8a)
+#
+
+#参考 https://developer.android.google.cn/ndk/guides/standalone_toolchain.html
+
+ANDROID_NDK_HOME=/home/mylhyz/Android/Sdk/ndk-bundle
+#NDK环境变量
+NDK=$ANDROID_NDK_HOME
+#生成文件安装路径
+PREFIX=$(pwd)/build/android
+#交叉编译工具链
+TOOLCHAIN=$NDK/toolchains/aarch64-linux-android-4.9/prebuilt/linux-x86_64
+#交叉编译头文件和库文件搜索路径
+SYSROOT=$NDK/platforms/android-21/arch-arm64
+#交叉编译器命名前缀
+CROSS_PREFIX=$TOOLCHAIN/bin/aarch64-linux-android-
+
+build_configure(){
+
+$(pwd)/ffmpeg-3.3.5/configure \
+	--prefix=$PREFIX \
+	--sysroot=$SYSROOT \
+	--enable-cross-compile \
+	--cross-prefix=$CROSS_PREFIX \
+	--target-os=linux \
+	--arch=arm64 \
+	--enable-gpl \
+	--enable-version3 \
+	--disable-symver \
+	--enable-shared \
+	--disable-programs \
+	--disable-doc \
+
+}
+
+echo $PREFIX
+echo $NDK
+echo $TOOLCHAIN
+echo $CROSS_PREFIX
+echo $SYSROOT
+
+build_configure
